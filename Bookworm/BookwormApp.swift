2421c58bc93a82eee9df03eac659296eee1fe241
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-12-03 11:36:15 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Dmitry Sharabin on 03.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
